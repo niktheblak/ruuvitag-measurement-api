30e91b0047b65262f35dcbb5bfaeb746dd81b6f3
Niko Korhonen <1004138+niktheblak@users.noreply.github.com>
2020-12-01 13:46:54 +0200

    Added test scripts

---
+++ scripts/influxdb_start.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+docker run \
+  -it \
+  --rm \
+  --name influxdb \
+  -p 8086:8086 \
+  -v "$PWD/influxdb/config/influxdb.conf:/etc/influxdb/influxdb.conf:ro" \
+  -v "$PWD/influxdb/data:/var/lib/influxdb" \
+  -e INFLUXDB_DB=ruuvitag \
+  -e INFLUXDB_ADMIN_USER=admin -e INFLUXDB_ADMIN_PASSWORD=admin \
+  -e INFLUXDB_USER=api -e INFLUXDB_USER_PASSWORD=api \
+  influxdb -config /etc/influxdb/influxdb.conf
