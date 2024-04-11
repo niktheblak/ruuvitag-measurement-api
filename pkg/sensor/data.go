1139834cc7be60d1f0d1dab1cdcba41e67efe923
Niko Korhonen <1004138+niktheblak@users.noreply.github.com>
2024-04-11 13:51:58 +0300

    chore: omit empty fields from response

--- pkg/sensor/data.go
+++ pkg/sensor/data.go
@@ -6,17 +6,17 @@ import (
 
 type Data struct {
 	Addr              string    `json:"mac"`
-	Name              string    `json:"name"`
+	Name              string    `json:"name,omitempty"`
 	Temperature       float64   `json:"temperature"`
 	Humidity          float64   `json:"humidity"`
 	DewPoint          float64   `json:"dew_point,omitempty"`
 	Pressure          float64   `json:"pressure"`
 	BatteryVoltage    float64   `json:"battery_voltage,omitempty"`
 	TxPower           int       `json:"tx_power,omitempty"`
-	AccelerationX     int       `json:"acceleration_x"`
-	AccelerationY     int       `json:"acceleration_y"`
-	AccelerationZ     int       `json:"acceleration_z"`
-	MovementCounter   int       `json:"movement_counter"`
-	MeasurementNumber int       `json:"measurement_number"`
+	AccelerationX     int       `json:"acceleration_x,omitempty"`
+	AccelerationY     int       `json:"acceleration_y,omitempty"`
+	AccelerationZ     int       `json:"acceleration_z,omitempty"`
+	MovementCounter   int       `json:"movement_counter,omitempty"`
+	MeasurementNumber int       `json:"measurement_number,omitempty"`
 	Timestamp         time.Time `json:"ts"`
 }
