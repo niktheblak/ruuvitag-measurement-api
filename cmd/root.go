f1e2472bcd0d20ddbd0d311a9fbbba10a53f32f7
Niko Korhonen <1004138+niktheblak@users.noreply.github.com>
2024-04-12 15:00:58 +0300

    feat: configurable log level

--- cmd/root.go
+++ cmd/root.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"log/slog"
+	"os"
 	"strings"
 
 	"github.com/spf13/cobra"
@@ -9,14 +10,28 @@ import (
 )
 
 var (
-	cfgFile string
-	logger  *slog.Logger
+	cfgFile  string
+	logLevel string
+	logger   *slog.Logger
 )
 
 var rootCmd = &cobra.Command{
 	Use:          "temperature-api",
 	Short:        "API for reading current environment measurements from InfluxDB",
 	SilenceUsage: true,
+	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+		var level = new(slog.LevelVar)
+		if err := level.UnmarshalText([]byte(viper.GetString("loglevel"))); err != nil {
+			return err
+		}
+		h := slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: level})
+		logger = slog.New(h)
+		if viper.ConfigFileUsed() != "" {
+			logger.LogAttrs(nil, slog.LevelInfo, "Using config file", slog.String("config", viper.ConfigFileUsed()))
+		}
+		logger.Info("Using log level", "level", level)
+		return nil
+	},
 }
 
 func Execute() error {
@@ -24,9 +39,15 @@ func Execute() error {
 }
 
 func init() {
-	logger = slog.Default()
 	cobra.OnInitialize(initConfig)
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.temperature-api.toml)")
+
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file path")
+	rootCmd.PersistentFlags().StringVar(&logLevel, "loglevel", "info", "log level")
+
+	if err := viper.BindPFlags(rootCmd.PersistentFlags()); err != nil {
+		panic(err)
+	}
+	viper.SetDefault("loglevel", "info")
 }
 
 func initConfig() {
@@ -37,11 +58,10 @@ func initConfig() {
 		viper.AddConfigPath("/etc/temperature-api")
 		viper.AddConfigPath("$HOME/.temperature-api")
 		viper.SetConfigName("config")
-		viper.SetConfigType("toml")
 	}
 	viper.AutomaticEnv()
 	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
-	if err := viper.ReadInConfig(); err == nil {
-		logger.LogAttrs(nil, slog.LevelInfo, "Using config file", slog.String("config", viper.ConfigFileUsed()))
+	if err := viper.ReadInConfig(); err != nil {
+		slog.Default().LogAttrs(nil, slog.LevelInfo, "Could not read config file, using only command line options", slog.String("config", viper.ConfigFileUsed()))
 	}
 }
