4aa31d85197e1742b8346fc3e81b28c0a16bcaff
Niko Korhonen <1004138+niktheblak@users.noreply.github.com>
2026-06-16 17:44:00 +0300

    feat: update response json schema to be more parseable
    BREAKING CHANGE: json response format changes

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -8,6 +8,7 @@ import (
 	"log/slog"
 	"net/http"
 	"regexp"
+	"slices"
 	"strconv"
 	"strings"
 	"time"
@@ -26,29 +27,31 @@ func latestHandler(service ruuvitag.Service, columnMap map[string]string, logger
 			http.Error(w, "Invalid timezone", http.StatusBadRequest)
 			return
 		}
-		n := 1
-		if r.URL.Query().Get("n") != "" {
+		count := 1
+		if r.URL.Query().Get("count") != "" {
 			val, err := strconv.ParseInt(r.URL.Query().Get("n"), 10, 32)
 			if err != nil || val < 1 {
 				http.Error(w, "Invalid n", http.StatusBadRequest)
 				return
 			}
-			n = int(val)
+			count = int(val)
 		}
 		columns, err := parseCSV(r.URL.Query().Get("columns"))
 		if err != nil {
 			http.Error(w, "Invalid columns", http.StatusBadRequest)
 			return
 		}
+		slices.Sort(columns)
 		names, err := parseCSV(r.URL.Query().Get("names"))
 		if err != nil {
 			http.Error(w, "Invalid names", http.StatusBadRequest)
 			return
 		}
+		slices.Sort(names)
 		logger.LogAttrs(r.Context(), slog.LevelDebug, "RuuviTags and columns from query", slog.Any("columns", columns), slog.Any("names", names))
 		ctx, cancel := context.WithTimeout(r.Context(), 5*time.Second)
 		defer cancel()
-		measurements, err := service.Latest(ctx, n, columns, names)
+		measurements, err := service.Latest(ctx, count, columns, names)
 		switch {
 		case errors.Is(err, context.DeadlineExceeded):
 			logger.LogAttrs(r.Context(), slog.LevelError, "Timeout while querying measurements", slog.Any("error", err))
@@ -65,15 +68,9 @@ func latestHandler(service ruuvitag.Service, columnMap map[string]string, logger
 		}
 		w.Header().Set("Content-Type", "application/json")
 		w.Header().Set("Cache-Control", "no-store, max-age=0")
-		var response any
-		if n == 1 {
-			// flatten the map to a single measurement per column for easier JSON readability
-			response = newest(measurements, columnMap, loc)
-		} else {
-			// return measurements as a map of lists with a maximum of n measurements per column
-			response = newestN(measurements, n, columnMap, loc)
-		}
-		if err := json.NewEncoder(w).Encode(response); err != nil {
+		resp := newest(measurements, count, columnMap, loc)
+		resp.Columns = columns
+		if err := json.NewEncoder(w).Encode(resp); err != nil {
 			logger.LogAttrs(r.Context(), slog.LevelError, "Error while writing output", slog.Any("error", err))
 			return
 		}
@@ -100,29 +97,35 @@ func parseCSV(values string) ([]string, error) {
 	return strings.Split(values, ","), nil
 }
 
-func newest(measurements map[string][]sensor.Fields, columnMap map[string]string, loc *time.Location) map[string]map[string]any {
-	response := make(map[string]map[string]any)
-	for k, m := range measurements {
-		if len(m) == 0 {
-			continue
-		}
-		fields := m[0]
-		fields.Timestamp = fields.Timestamp.In(loc)
-		response[k] = columnmap.TransformFields(columnMap, fields)
-	}
-	return response
+type response struct {
+	Timezone string       `json:"tz"`
+	Columns  []string     `json:"columns"`
+	Series   []seriesItem `json:"series"`
 }
 
-func newestN(measurements map[string][]sensor.Fields, n int, columnMap map[string]string, loc *time.Location) map[string][]map[string]any {
-	response := make(map[string][]map[string]any)
+type seriesItem struct {
+	Name         string           `json:"name"`
+	MAC          string           `json:"mac"`
+	Measurements []map[string]any `json:"measurements"`
+}
+
+func newest(measurements map[string][]sensor.Fields, count int, columnMap map[string]string, loc *time.Location) response {
+	resp := response{
+		Timezone: loc.String(),
+	}
 	for k, ms := range measurements {
+		item := seriesItem{
+			Name: k,
+		}
 		for i, m := range ms {
-			if i == n {
+			if i == count {
 				break
 			}
+			item.MAC = *m.Addr
 			m.Timestamp = m.Timestamp.In(loc)
-			response[k] = append(response[k], columnmap.TransformFields(columnMap, m))
+			item.Measurements = append(item.Measurements, columnmap.TransformFields(columnMap, m))
 		}
+		resp.Series = append(resp.Series, item)
 	}
-	return response
+	return resp
 }
